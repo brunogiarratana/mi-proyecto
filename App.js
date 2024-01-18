9f6bef0ed0b0ec2386f5a4b32ba4b05afb7c1821
brunogiarratana <brunogiarra@gmail.com>
2024-01-18 20:53:09 -0300

    first

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hola, Coder!</Text>
     </View>
   );
 }
@@ -13,8 +12,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "black",
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  text: {
+    fontSize: 24,
+    color:'white',
   },
 });
